51802c452af6f88a35c18f4cc00563acee2442af
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-02-16 06:19:52 +0900

    15.1

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
-export default function LogIn() {
+export default function LogIn({ navigation }) {
   return (
     <View>
       <Text>LogIn</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
+        <Text>Go to Create Acount</Text>
+      </TouchableOpacity>
     </View>
   );
 }
