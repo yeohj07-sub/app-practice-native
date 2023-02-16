fa19d74a01f5dbcd078c075f1300b4a0cc66087e
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-02-16 09:04:53 +0900

    15.4

--- App.js
+++ App.js
@@ -3,8 +3,7 @@ import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
 import { Asset } from "expo-asset";
 import LoggedOutNav from "./navigators/LoggedOutNav";
-import { NavigationContainer, ThemeProvider } from "@react-navigation/native";
-import { Appearance } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -28,9 +27,6 @@ export default function App() {
       onFinish();
     }
   }
-  const subscription = Appearance.addChangeListener(({ colorScheme }) => {
-    console.log(colorScheme);
-  });
   return (
     <NavigationContainer>
       <LoggedOutNav />
--- navigators/LoggedOutNav.js
+++ navigators/LoggedOutNav.js
@@ -9,7 +9,11 @@ const Stack = createNativeStackNavigator();
 export default function LoggedOutNav() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen
+        name="Welcome"
+        options={{ headerShown: false }}
+        component={Welcome}
+      />
       <Stack.Screen name="LogIn" component={LogIn} />
       <Stack.Screen name="CreateAccount" component={CreateAccount} />
     </Stack.Navigator>
