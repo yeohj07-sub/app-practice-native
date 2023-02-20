b40a6a986de162f81dca23494571f5e173f5de2a
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-02-20 23:11:03 +0900

    16.9

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,6 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
-import React from "react";
-import { FlatList, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 import Photo from "../components/Photo";
 import ScreenLayout from "../components/ScreenLayout";
 import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../fragments";
@@ -26,13 +26,21 @@ const FEED_QUERY = gql`
 `;
 
 export default function Feed({ navigation }) {
-  const { data, loading } = useQuery(FEED_QUERY);
+  const { data, loading, refetch } = useQuery(FEED_QUERY);
   const renderPhoto = ({ item: photo }) => {
     return <Photo {...photo} />;
   };
+  const refresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+  const [refreshing, setRefreshing] = useState(false);
   return (
     <ScreenLayout loading={loading}>
       <FlatList
+        refreshing={refreshing}
+        onRefresh={refresh}
         style={{ width: "100%" }}
         showsVerticalScrollIndicator={false}
         data={data?.seeFeed}
