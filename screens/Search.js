3d4f86ea07e509bf3b3cde5d1a043445ed6c56bb
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-02-23 05:09:48 +0900

    17.3

--- screens/Search.js
+++ screens/Search.js
@@ -1,19 +1,44 @@
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { Text, TextInput, View } from "react-native";
+import styled from "styled-components/native";
+import DissmissKeyboard from "../components/DismissKeyboard";
+
+const Input = styled.TextInput``;
 
 export default function Search({ navigation }) {
+  const { setValue, register, watch } = useForm();
+  const SearchBox = () => (
+    <TextInput
+      style={{ backgroundColor: "white" }}
+      placeholderTextColor="black"
+      placeholder="Search photos"
+      autoCapitalize="none"
+      returnKeyLabel="Search"
+      returnKeyType="search"
+      autoCorrect={false}
+      onChangeText={(text) => setValue("keyword", text)}
+    />
+  );
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: SearchBox,
+    });
+    register("keyword");
+  }, []);
+  console.log(watch());
   return (
-    <View
-      style={{
-        backgroundColor: "black",
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <TouchableOpacity onPress={() => navigation.navigate("Photo")}>
+    <DissmissKeyboard>
+      <View
+        style={{
+          backgroundColor: "black",
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
         <Text style={{ color: "white" }}>Photo</Text>
-      </TouchableOpacity>
-    </View>
+      </View>
+    </DissmissKeyboard>
   );
 }
