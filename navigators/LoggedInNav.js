b9517e17ec8c29d0dac16938e62476a2cebcf5ec
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-02-24 11:43:39 +0900

    18.1

--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import TabsNav from "./TabsNav";
-import Upload from "../screens/Upload";
+import UploadNav from "./UploadNav";
 
 const Stack = createStackNavigator();
 
@@ -11,7 +11,7 @@ export default function LoggedInNav() {
       screenOptions={{ headerShown: false, presentation: "modal" }}
     >
       <Stack.Screen name="Tabs" component={TabsNav} />
-      <Stack.Screen name="Upload" component={Upload} />
+      <Stack.Screen name="Upload" component={UploadNav} />
     </Stack.Navigator>
   );
 }
