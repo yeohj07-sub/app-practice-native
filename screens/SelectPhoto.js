a41c4014cdc350c76b45a51bff072a7b0585bc93
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-02-26 02:10:36 +0900

    18.13

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -81,7 +81,7 @@ export default function SelectPhoto({ navigation }) {
     navigation.setOptions({
       headerRight: HeaderRight,
     });
-  });
+  }, [chosenPhoto]);
   const numColumns = 4;
   const { width } = useWindowDimensions();
   const choosePhoto = (uri) => {
