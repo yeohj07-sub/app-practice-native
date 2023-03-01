b3abc238948f75177d74ef0f09551190e750612b
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-03-01 19:59:08 +0900

    19.3

--- navigators/MessagesNav.js
+++ navigators/MessagesNav.js
@@ -16,12 +16,17 @@ export default function MessagesNav() {
         headerStyle: {
           backgroundColor: "black",
         },
-        headerBackImage: ({ tintColor }) => (
-          <Ionicons color={tintColor} name="chevron-down" size={28} />
-        ),
       }}
     >
-      <Stack.Screen name="Rooms" component={Rooms} />
+      <Stack.Screen
+        name="Rooms"
+        options={{
+          headerBackImage: ({ tintColor }) => (
+            <Ionicons color={tintColor} name="chevron-down" size={30} />
+          ),
+        }}
+        component={Rooms}
+      />
       <Stack.Screen name="Room" component={Room} />
     </Stack.Navigator>
   );
