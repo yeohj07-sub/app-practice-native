2f4d46d8f25450712e74e4fb317c14ca4e526107
yeohj0710 <yeohj0710@yonsei.ac.kr>
2023-03-04 02:13:41 +0900

    19.11

--- screens/Room.js
+++ screens/Room.js
@@ -1,4 +1,10 @@
-import { gql, useMutation, useQuery, useSubscription } from "@apollo/client";
+import {
+  gql,
+  useApolloClient,
+  useMutation,
+  useQuery,
+  useSubscription,
+} from "@apollo/client";
 import React, { useEffect } from "react";
 import { FlatList, KeyboardAvoidingView, View } from "react-native";
 import ScreenLayout from "../components/ScreenLayout";
@@ -147,6 +153,38 @@ export default function Room({ route, navigation }) {
       id: route?.params?.id,
     },
   });
+  const client = useApolloClient();
+  const updateQuery = (prevQuery, options) => {
+    const {
+      subscriptionData: {
+        data: { roomUpdates: message },
+      },
+    } = options;
+    if (message.id) {
+      const messageFragment = client.cache.writeFragment({
+        fragment: gql`
+          fragment NewMessage on Message {
+            id
+            payload
+            user {
+              username
+              avatar
+            }
+            read
+          }
+        `,
+        data: message,
+      });
+      client.cache.modify({
+        id: `Room:${route.params.id}`,
+        fields: {
+          messages(prev) {
+            return [...prev, messageFragment];
+          },
+        },
+      });
+    }
+  };
   useEffect(() => {
     if (data?.seeRoom) {
       subscribeToMore({
@@ -154,6 +192,7 @@ export default function Room({ route, navigation }) {
         variables: {
           id: route?.params?.id,
         },
+        updateQuery,
       });
     }
   }, [data]);
